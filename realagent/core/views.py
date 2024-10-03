8ead534df1ce8fdc532395c703969a0eaf830624
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-03 18:54:43 +0100

    modified url to display image and rendered some data from the database on to the home page

--- realagent/core/views.py
+++ realagent/core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from listing.models import Category, Location, Type, Listing
 
 # Create your views here.
 def index(request):
-    return render(request, "core/index.html")
+    listings = Listing.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, "core/index.html", {
+        'categories': categories,
+        'listings': listings 
+    })
