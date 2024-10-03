d3b07cfdb4769c240fc697e9573451d803f833a8
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-03 05:36:41 +0100

    added listing tag for Type and registered it

--- realagent/listing/models.py
+++ realagent/listing/models.py
@@ -22,3 +22,14 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+# Type model
+class Type(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Types"
+
+    def __str__(self):
+        return self.name
