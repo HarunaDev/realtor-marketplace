f4a01de3861a2ec08a2005f346b2cec00b334414
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-03 06:50:50 +0100

    registered model in admin site

--- realagent/listing/admin.py
+++ realagent/listing/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Location, Type
+from .models import Category, Location, Type, Listing
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Location)
 admin.site.register(Type)
+admin.site.register(Listing)
