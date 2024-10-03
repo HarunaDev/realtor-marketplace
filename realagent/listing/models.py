a2a613a6b40afd620d1925765d26a1526264dc40
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-03 05:29:49 +0100

    added listing tag for Location

--- realagent/listing/models.py
+++ realagent/listing/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# Category model
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
         ordering = ('name',)
         verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+    
+# Location model
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Locations"
 
     def __str__(self):
         return self.name
