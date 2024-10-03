3c04abcc1a77aa3285d7f82022ae64791c661e73
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-03 06:47:05 +0100

    created model for each property listing

--- realagent/listing/models.py
+++ realagent/listing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # Category model
@@ -33,3 +34,17 @@ class Type(models.Model):
 
     def __str__(self):
         return self.name
+
+# Listing model
+class Listing(models.Model):
+    category = models.ForeignKey(Category, related_name='apartments', on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, related_name='apartments', on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, related_name='apartments', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='apartments', on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='listing_media', blank=True, null=True)
+    large = models.ImageField(upload_to='listing_media', blank=True, null=True)
