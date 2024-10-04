04b9cd0247d845c929374d0dfd8cdd397bfaca5a
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-04 11:19:57 +0100

    created urls file and modified main urls file
    created forms.py file to handle login and sign up

--- realagent/realagent/urls.py
+++ realagent/realagent/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', include('core.urls')),
     path('listings/', include('listing.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
