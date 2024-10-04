3fabf5327f5aefca63de0c60bb9c15956a9a6440
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-04 03:45:55 +0100

    modified details page to show related items based on location

--- realagent/listing/views.py
+++ realagent/listing/views.py
@@ -5,7 +5,9 @@ from .models import Listing
 # create view for apartment detail
 def detail(request, pk):
     listing = get_object_or_404(Listing, pk=pk)
+    related_items = Listing.objects.filter(location=listing.location, is_sold=False).exclude(pk=pk)[:3]
 
     return render(request, 'listing/detail.html', {
-        'listing': listing
+        'listing': listing,
+        'related_items': related_items
     })
