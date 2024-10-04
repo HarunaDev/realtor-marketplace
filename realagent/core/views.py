5ec8f25c30855768bbf344b70f82278f0866b217
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-04 11:41:13 +0100

    created signup template & view and added url

--- realagent/core/views.py
+++ realagent/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from listing.models import Category, Location, Type, Listing
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,11 @@ def index(request):
     return render(request, "core/index.html", {
         'categories': categories,
         'listings': listings 
+    })
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
     })
