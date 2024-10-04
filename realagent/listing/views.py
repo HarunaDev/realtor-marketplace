f4db7871dddaf7508cb56a90ac513d5adc9c9917
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-04 02:13:21 +0100

    created views for property detail with template and added urls to main url patterns
    added link to html file

--- realagent/listing/views.py
+++ realagent/listing/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Listing
 # Create your views here.
+
+# create view for apartment detail
+def detail(request, pk):
+    listing = get_object_or_404(Listing, pk=pk)
+
+    return render(request, 'listing/detail.html', {
+        'listing': listing
+    })
