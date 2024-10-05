83873833d02ca5c17a81b26eab2db50abac13406
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-05 03:01:55 +0100

    added password change url paths

--- realagent/core/urls.py
+++ realagent/core/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', views.logout_view, name='logout'),  # Ensure the logout_view exists in views.py
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='core/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='core/password_change_done.html'), name='password_change_done'),
 ]
