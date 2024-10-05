eeeeca96bdadea07676e22aa97dc8984ceac6efd
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-05 09:37:58 +0100

    modified url to be directed to the password_change_done template to fix the bug

--- realagent/core/urls.py
+++ realagent/core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 from .forms import LoginForm
@@ -10,6 +10,13 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', views.logout_view, name='logout'),  # Ensure the logout_view exists in views.py
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='core/password_change.html'), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='core/password_change_done.html'), name='password_change_done'),
+    path('password_change/', 
+         auth_views.PasswordChangeView.as_view(
+             template_name='core/password_change.html',
+             success_url=reverse_lazy('core:password_change_done')
+         ), 
+         name='password_change'),
+    path('password_change_done/', 
+         auth_views.PasswordChangeDoneView.as_view(template_name='core/password_change_done.html'), 
+         name='password_change_done')
 ]
