ee3d0b723cee00c210800ee0342ccc5e1fb9d883
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-06 12:52:08 +0100

    added url for password reset teplates

--- realagent/core/urls.py
+++ realagent/core/urls.py
@@ -18,5 +18,22 @@ urlpatterns = [
          name='password_change'),
     path('password_change_done/', 
          auth_views.PasswordChangeDoneView.as_view(template_name='core/password_change_done.html'), 
-         name='password_change_done')
+         name='password_change_done'),
+
+    
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='core/password_reset.html',
+             success_url=reverse_lazy('core:password_reset_done')
+         ), 
+         name='password_reset'),
+    path('password_reset_done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'), 
+         name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='core/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    path('password_reset_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='core/password_reset_complete.html'), 
+         name='password_reset_complete'),
 ]
