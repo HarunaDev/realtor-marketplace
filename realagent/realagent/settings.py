b8a37c7fe13193ecaa6d297b154c0511a9e7b773
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-12 04:52:28 +0100

    modified files to integrate tailwind css

--- realagent/realagent/settings.py
+++ realagent/realagent/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'listing',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+TAILWIND_APP_NAME = 'theme'
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
