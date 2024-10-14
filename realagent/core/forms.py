700c37f56c3db046c8887867c7e5bf1e43631384
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-14 09:54:21 +0100

    styled signup page
    issue with form input outline colors

--- realagent/core/forms.py
+++ realagent/core/forms.py
@@ -8,12 +8,12 @@ class LoginForm(AuthenticationForm):
     """
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your username',  # Placeholder text for the username field
-        'class': 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'  # Tailwind CSS classes for styling
+        'class': 'block w-full rounded-md border-2 border-[#1d1d1d] py-1.5 text-gray-900 shadow-sm ring-1 ring-[#1d1d1d] placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-[#1d1d1d] sm:text-sm sm:leading-6'  # Tailwind CSS classes for styling
     }))
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Your password',  # Placeholder text for the password field
-        'class': 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'  # Tailwind CSS classes for styling
+        'class': 'block w-full rounded-md border-2 border-[#1d1d1d] py-1.5 text-gray-900 shadow-sm ring-1 ring-[#1d1d1d] placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-[#1d1d1d] sm:text-sm sm:leading-6'  # Tailwind CSS classes for styling
     }))
 
 class SignupForm(UserCreationForm):
