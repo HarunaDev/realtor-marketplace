d7064962c6f382884cb22099aaa7780a67f0f6b4
Alvin Haruna <harunaalvin69@gmail.com>
2024-10-17 20:18:47 +0100

    created form model for editing listing

--- realagent/listing/forms.py
+++ realagent/listing/forms.py
@@ -4,4 +4,9 @@ from .models import Listing
 class NewListingForm(forms.ModelForm):
     class Meta:
         model = Listing
-        fields = ['category', 'location', 'type', 'title', 'description', 'price', 'thumbnail', 'large']
+        fields = ('category', 'location', 'type', 'title', 'description', 'price', 'thumbnail', 'large')
+
+class EditListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ('category', 'type', 'title', 'description', 'price', 'thumbnail', 'large', 'is_sold')
